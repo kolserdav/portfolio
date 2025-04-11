83e65b12c92d969779f00b3917433e20f88d8ad6
kolserdav <serega12101983@gmail.com>
2025-04-11 16:31:34 +0700

    add open graph tags

--- components/Head.tsx
+++ components/Head.tsx
@@ -14,6 +14,11 @@ const Head = ({
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="/images/kolserdav.png" />
+      <meta property="og:url" content="/" />
+      <meta property="og:type" content="website" />
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
