35cadb7d6d8402595d9e67b8dba34d6343f5367c
kolserdav <serega12101983@gmail.com>
2025-04-11 16:50:34 +0700

    add twitter meta

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,5 +1,7 @@
 import NextHead from 'next/head';
 
+const IMAGE_URL = '/images/kolserdav.png';
+
 const Head = ({
   title,
   description,
@@ -16,9 +18,13 @@ const Head = ({
       <meta name="keywords" content={keywords} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content="/images/kolserdav.png" />
+      <meta property="og:image" content={IMAGE_URL} />
       <meta property="og:url" content="/" />
       <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={IMAGE_URL} />
       <link rel="icon" href="/favicon.ico" />
     </NextHead>
   );
